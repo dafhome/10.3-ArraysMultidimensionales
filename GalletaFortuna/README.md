4a8cf57c80b09b36cd015d197abf33117a3649a2
dafhome <d.fernalvarez@gmail.com>
2024-03-13 20:02:22 +0100

    commit-tablas-multiplicar

---
+++ GalletaFortuna/README.md
@@ -0,0 +1,11 @@
+# 10.3-ArraysMultidimensionales
+ 
+3- LA GALLETA DE LA FORTUNA
+Tienes dos arrays,
+- uno con los nombres de los estudiantes de este curso
+- Y otro con galletas con mensajes de ‘vaticinio’, la cantidad que tú quieras (no hace falta que sea la misma
+cantidad que estudiantes).
+Debes buscar ALEATORIAMENTE un estudiante, y una frase de vaticinio,
+Y mostrar el resultado del tipo:
+"vaticinio para Ferdi:
+Este fin de semana debes de escuchar tus sueños, porque contienen la clave para encontrar un tesoro cerca de tu casa"
---
+++ TablasMultiplicar/main.js
@@ -0,0 +1,25 @@
+// 1- TABLAS DE MULTIPLICAR
+// El usuario entra un intervalo de dos números entre el 1 y el 10 (el segundo número debe ser mayor
+// que el primero).
+// El programa muestra en orden, con un título, la tabla de multiplicar del primer número, y luego el siguiente, hasta
+// el último.
+// Ej: si entra 5 y 7, mostrará las tablas del 5, del 6 y del 7.
+
+let menor;
+let mayor;
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+// function tablas 
+function tablasMultiplicar(){
+    menor = parseInt(prompt("Introduce el primer número entre 1 y 10"));
+    mayor = parseInt(prompt("Introduce el segundo número entre 1 y 10, mayor que el AuthenticatorAssertionResponse."));
+    document.getElementById("tablas").innerHTML = "";
+    for (menor; menor<mayor+1; menor++){
+        document.getElementById("tablas").innerHTML += "Tabla del "+menor+": ";
+        for (let i=0; i<10; i++){
+            document.getElementById("tablas").innerHTML += i*menor+" ";
+        }
+        document.getElementById("tablas").innerHTML += "<br>";
+    }
+}
